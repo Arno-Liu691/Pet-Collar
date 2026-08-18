aac6b118b793d5edc78f59b8289c8a88b4c34f2e
Arno-Liu691 <arnoliu2333@gmail.com>
2026-08-18 01:50:36 +0100

    Delete Dataset directory

--- Dataset/IMU_Movement_Sensor_Behavior_Classification_Mendeley/AnalysisCode/Code2pub/Tree_ClassCrit_Back.m
+++
@@ -1,9 +0,0 @@
-function criterion = Tree_ClassCrit_Back(XTRAIN,ytrain,XTEST,ytest)
-
-
-MaxSplits = 124; % esioptimoinnista rautalankana
-tree = fitctree(XTRAIN,ytrain, ...
-    'MaxNumSplits',MaxSplits,'CrossVal','off');
-
-Yh =  predict(tree,XTEST);
-criterion = sum(Yh~=ytest);
--- Dataset/IMU_Movement_Sensor_Behavior_Classification_Mendeley/AnalysisCode/Code2pub/Tree_ClassCrit_Neck.m
+++
@@ -1,9 +0,0 @@
-function criterion = Tree_ClassCrit_Neck(XTRAIN,ytrain,XTEST,ytest)
-
-
-MaxSplits = 121; % esioptimoinnista rautalankana
-tree = fitctree(XTRAIN,ytrain, ...
-    'MaxNumSplits',MaxSplits,'CrossVal','off');
-
-Yh =  predict(tree,XTEST);
-criterion = sum(Yh~=ytest);
